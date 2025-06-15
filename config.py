678c644be3c71630fc81612ebbc818cd10dde03c
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2025-06-15 12:25:06 +0500

    config, aiogram, requirements

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+BOT_API_KEY = config.get("BOT_API_KEY")
+ADMIN_ID = config.get("ADMIN_ID")
+MONGO_DB_USERNAME = config.get("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = config.get("MONGO_DB_PASSWORD")
